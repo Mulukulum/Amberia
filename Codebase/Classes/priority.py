2ddaf6647c3c2abc880416824e115937dfee7ef2
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-09 22:40:03 +0530

    Update priority.py

--- Codebase/Classes/priority.py
+++ Codebase/Classes/priority.py
@@ -7,9 +7,11 @@
 #Each task contains one of these, each object has a prioritylevel and a color associated with it
 #There can be a maximum of 10 priority levels, where 10 is the lowest and 1 in the highest
 
-class Priority:
-    #Sets a tuple containing whole numbers from 1 to 10 
-    ValidPriorites=(x+1 for x in range(10))
+
+
+class Priority: 
+    
+    ValidPriorites=(1,2,3,4,5,6,7,8,9,10)                 #Sets a tuple containing whole numbers from 1 to 10
     
     #Initialising class
     def __init__(self,PriorityLevel=10) -> None:
@@ -19,27 +21,35 @@ class Priority:
     def __repr__(self) -> str:
         return f'Priority Level {self.PriorityLevel}'
     
-    #This classmethod returns the color of a priority level 
+    #This classmethod returns the color of a priority level either as an integer
 
     @classmethod
-    def GetColor(cls,PrLevel) -> int :
-        import csv
-        from os import path #To Get the Path of the directory where code is stored
+    def GetColor(cls,PrLevel,ReturnHex=False) -> int :
+        from csv import reader,QUOTE_NONE
+        QUOTE_NONE                                      #Instructing the reader to Quote Nothing
+        from os import path                                 #To Get the Path of the directory where code is stored
         FilePath=path.dirname(__file__) + '\\Priority.csv'  #This gets the path of the csv file
-        csv.QUOTE_NONE #Instructing the reader to Quote Nothing
-        with open(FilePath,'r+') as CSVFile:
-            Reader=csv.reader(CSVFile,delimiter=',',skipinitialspace=True)
-            L=list(Reader)
-
+        with open(FilePath,'r+') as CSVFile:                
+            Reader=reader(CSVFile                      #Open the CSV File
+            ,delimiter=','
+            ,skipinitialspace=True)
+            try:
+                Color= list(Reader)[PrLevel]                #This gets the color that is stored as an integer
+            except IndexError:
+                #Place for ErrorLog Function Call
+                return None
+        return Color
+        
+            
 
     #Method to update priority level of said priority object
     def UpdatePriorityLevel(self,NewLevel):
         if NewLevel in Priority.ValidPriorities:
             self.PriorityLevel=NewLevel 
-            ...                                #This updates the color as well
+            ...                                #This updates the color as well (inprog)
         else:
             return False
         return True
-Priority.GetColor(2)
+#Priority.GetColor(2)
     
         
