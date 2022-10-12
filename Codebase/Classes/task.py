ec4d773ea1c0399a294a80d0fe4c5065249bc377
shyam-k123 <shyamsschoolemail@gmail.com>
2022-10-12 20:03:15 +0530

    Update task.py

--- Codebase/Classes/task.py
+++ Codebase/Classes/task.py
@@ -26,20 +26,17 @@ class task:
         return f"task('{self.TaskTitle}','{self.TaskDesc}',{self.priority},{self.DueDate},{self.Labels})" 
         #Repr returns how to create the task
     
-    def reconfigure(self,TaskTitle, TaskDesc=None, priority=None, DueDate=None, Labels=None):
-        self.TaskTitle=TaskTitle                        #Changes the title to a newly provided title
-        self.TaskDesc=TaskDesc                          #Changes the desc to a newly provided desc
-        self.DueDate=DueDate                            #Changes the due date to a newly provided due date
+    def reconfigure(self,TaskTitle=None, TaskDesc=None, priority=None, DueDate=None, Labels=None):
+        if TaskTitle!=None:
+            self.TaskTitle=TaskTitle                    #Changes the title to a newly provided title
+        if TaskDesc!=None:
+            self.TaskDesc=TaskDesc                      #Changes the desc to a newly provided desc
+        if DueDate!=None:
+            self.DueDate=DueDate                        #Changes the due date to a newly provided due date
         if Priority.IsValidPriority(priority):          #Checks if the incoming argument is a valid priority level
-            self.priority=Priority(priority)            #If so, then give the task its priority
-        else:
-            self.priority=Priority(10)                  #If not, then set it to a default value of 10
-            #Line for error log
-
-        if Labels==None:                                #Checks if any labels are selected
-            self.Labels=[]                              #Makes it an empty list instead of None
-        else: 
-            self.Labels=list(Labels)      
+            self.priority=Priority(priority)            #If so, then give the task its new priority
+        if Labels!=None:
+            self.set_label(Labels)            
     
 
     
