e8879c36ef1baa92bbf6639ae03a1bcae9dd6da5
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-14 18:34:48 +0530

    Update label.py

--- Codebase/Classes/label.py
+++ Codebase/Classes/label.py
@@ -4,7 +4,6 @@ class label:
     #Class initialisation
     def __init__(self,Title='',Color=None) -> None:
         self.Title=Title                    #Set the Title of the Label
-        
 
     #Label Representation
     def __repr__(self) -> str:
