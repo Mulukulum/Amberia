a04115c38a318c496802dd23c7f903a1ef873230
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-30 12:00:50 +0530

    Update TaskManagement.py

--- TaskManagement.py
+++ TaskManagement.py
@@ -1,7 +1,6 @@
 
-
 class Section:
-    def __init__(self, name, project=None, tasks=[]):
+    def __init__(self, name, project=None, tasks=list()):
         self.name = name                 #Initialize name of section
         self.project = project
         self.tasks = tasks               #Initialize a list of tasks
@@ -16,23 +15,22 @@ class Section:
         self.tasks.remove(deltask)
 
     def display_tasks(self):             #Display the list of tasks
-        for t in self.tasks:
-            print(t)
+        print(*self.tasks,sep='\n')      #Displays tasks without a for loop         
 
     def __repr__(self):
-        return f"Section({self.name},{self.project},{self.tasks})"
+        return f"Section({self.name},{self.project},{self.tasks})"              
 
     def __str__(self):
-        return 'Section name: ' + self.name + '\nProject name: ' + self.project.name + \
-               '\nTasks: ' + str([str(t) for t in self.tasks])
+        return f'Section name: {self.name} \nProject name: {self.project.name} \nTasks: {[str(t) for t in self.tasks]}'        
+        #fstring returns the string representation
 
 
 class Project:
-    def __init__(self, name, color, projects = [], parentprojects = []):
+    def __init__(self, name, color=None, projects = list(), parentprojects = list()):
         self.name = name                 #Initialize name of project
         self.color = color               #Initialize display color
-        self.sections = []               #Initialize list of sections
-        defaultsection = Section("_projectname")
+        self.sections = list()               #Initialize list of sections
+        defaultsection = Section(f"_{self.name}")
         defaultsection.set_project(self)
         self.sections.append(defaultsection)
         self.subprojects = projects           #Initialize list of sub projects 
@@ -75,11 +73,10 @@ class Project:
             print(p)
 
     def __str__(self):
-        return 'Project name: ' + self.name + \
-               'Display color: ' + self.color + \
-               'Sections: ' + str([str(s) for s in self.sections])
+        return  f'Project name: {self.name} \
+                \nDisplay color: {self.color} \
+                \nSections: {[str(s) for s in self.sections]}'
 
     def __repr__(self):
         return f"Project({self.name},{self.color},{self.sections},{self.subprojects},{self.parentprojects})"
-
 
