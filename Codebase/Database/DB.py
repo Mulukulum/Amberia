b4b0c44a2816f52ac6d94f3e57d6caad04a74e57
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-30 17:11:49 +0530

    Update DB.py

--- Codebase/Database/DB.py
+++ Codebase/Database/DB.py
@@ -1,5 +1,10 @@
 import sqlite3
-Connector=sqlite3.connect(r"Data\\Amber.db")       #Creates the database file if it doesn't exist already
+
+from os.path import dirname
+FilePath=dirname(__file__)+"\Data\Amber.db"     #Gets the path of the database
+del dirname
+
+Connector=sqlite3.connect(FilePath)       #Creates the database file if it doesn't exist already
 Cursor=Connector.cursor()
 
 Connector.commit()
