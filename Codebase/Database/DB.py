480dd4c599a7572b002c3999f58e6d80df5326aa
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-30 14:03:00 +0530

    Update DB.py

--- Codebase/Database/DB.py
+++ Codebase/Database/DB.py
@@ -1,3 +1,6 @@
 import sqlite3
 Connector=sqlite3.connect("Amber.db")       #Creates the database file if it doesn't exist already
 Cursor=Connector.cursor()
+
+Connector.commit()
+Connector.close()
