2ec3556d5670af43a570b4cccaefd71d3893f1f1
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-30 17:08:32 +0530

    Created Initialization files

--- Codebase/Database/DB.py
+++ Codebase/Database/DB.py
@@ -1,5 +1,5 @@
 import sqlite3
-Connector=sqlite3.connect("Amber.db")       #Creates the database file if it doesn't exist already
+Connector=sqlite3.connect(r"Data\\Amber.db")       #Creates the database file if it doesn't exist already
 Cursor=Connector.cursor()
 
 Connector.commit()
--- Codebase/Database/Initialize.py
+++ Codebase/Database/Initialize.py
@@ -1,6 +1,11 @@
 import sqlite3
-Connector=sqlite3.connect("Amber.db")       #Creates the database file if it doesn't exist already
+
+from os.path import dirname
+FilePath=dirname(__file__)+"\Data\Amber.db"     #Gets the path of the database
+del dirname
+
+Connector=sqlite3.connect(FilePath)       #Creates the database file if it doesn't exist already and makes a connection
 Cursor=Connector.cursor()
-Cursor.execute('''CREATE DATABASE IF NOT EXISTS Projects;''')
+
 Connector.commit()
 Connector.close()
