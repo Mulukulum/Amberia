8a8141c36b8f68571c46f1c10dcacf92714d1e52
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-30 17:47:06 +0530

    Minor Changes

--- Codebase/Classes/label.py
+++ Codebase/Classes/label.py
@@ -1,9 +1,12 @@
 
-class label:
+class Label:
     #Class initialisation
     def __init__(self,Title='',Color=None) -> None:
         self.Title=Title                    #Set the Title of the Label
-
+        self.Color=Color
     #Label Representation
     def __repr__(self) -> str:
-        return f'Label {self.Title}'
+        return f'Label({self.Title},{self.Color})'
+    
+    def __str__(self) -> str:
+        return f'Label {self.Title} Color {self.Color}'
--- Codebase/Database/Initialize.py
+++ Codebase/Database/Initialize.py
@@ -4,8 +4,8 @@ from os.path import dirname
 FilePath=dirname(__file__)+"\Data\Amber.db"     #Gets the path of the database
 del dirname
 
-Connector=sqlite3.connect(FilePath)       #Creates the database file if it doesn't exist already and makes a connection
-Cursor=Connector.cursor()
+Con=sqlite3.connect(FilePath)       #Creates the database file if it doesn't exist already and makes a connection
+Cursor=Con.cursor()
 
-Connector.commit()
-Connector.close()
+Con.commit()
+Con.close()
