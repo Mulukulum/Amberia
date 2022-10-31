8faf8b482f1c3d851f08da18d84dc38417226e27
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-31 22:46:41 +0530

    Update Stuff

--- Codebase/Database/Functions/StartUp.py
+++ Codebase/Database/Functions/StartUp.py
@@ -1,7 +1,7 @@
 
 from Codebase.Database import DataBasePath
 #The following function checks if a table exists in a database
-def CheckAndCreate(cur,name,attributes=None):
+def CheckAndCreate(cur,name,attributes):
     cur.execute(f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{name}';""")        #Checks if the table exists
     if cur.fetchall()==[]:              #If No matches were found, (i.e table doesn't exist)
         if _CreateTable(cur,name,attributes)==None:
@@ -12,7 +12,7 @@ def CheckAndCreate(cur,name,attributes=None):
 #Function to create a table
 def _CreateTable(cur,name,attributes):
     try:
-        cur.execute(f"""CREATE TABLE {name}""")
+        cur.execute(f"""CREATE TABLE {name}({attributes})""")
     except:
         return False
 
--- Codebase/Database/Initialize.py
+++ Codebase/Database/Initialize.py
@@ -1,48 +1,83 @@
 import sqlite3
 from Codebase.Database import DataBasePath      #Gets the path of the database on the machine
 from Codebase.Database.Functions.StartUp import CheckAndCreate
+from Codebase.ErrorLogs.logging import Log,ErrorLog
+
+
+
 Con=sqlite3.connect(DataBasePath)       #Creates the database file if it doesn't exist already and makes a connection
 Cursor=Con.cursor()
 
 '''
 LIST OF TABLES
 
-The following is a list of all tables that should be present in the Database
+The following is a list of all tables and their attributes that should be present in the Database
 
 projects
 Attributes :-
-ID Text PRIMARY KEY
-ParentID Text
+id INTEGER PRIMARY KEY
+parentid INTEGER
+title TEXT NOT NULL
+color TEXT NOT NULL
+sectioncount INTEGER NOT NULL
+projectcount INTEGER NOT NULL
+projecttemplate INTEGER
 
-
-labels
+labels 
+There is no ID here because having Labels with the same titles is a bad idea
 Attributes:-
-
+title TEXT PRIMARY KEY
+color TEXT NOT NULL
+taskcount INTEGER NOT NULL                          
 
 tasks
+(parentid is same as sectionid for a regular task, sectionid can be null for a subtask)
 Attributes:-
-
+taskid INTEGER PRIMARY KEY
+title TEXT NOT NULL
+parentid INTEGER NOT NULL 
+sectionid INTEGER
+priority INTEGER NOT NULL
+color INTEGER NOT NULL
+tasktemplate INTEGER
 
 sections
 Attributes:-
+sectionid INTEGER PRIMARY KEY
+title TEXT NOT NULL
+taskcount INTEGER NOT NULL
+parentprojectid INTEGER NOT NULL
+sectiontemplate INTEGER
 
-
-
-
+colors
+Attributes:-
+level INTEGER PRIMARY KEY
+hexcode TEXT NOT NULL
 
 '''
+
 #Check if the Following Tables exist
 CheckList=('projects',
             'labels',
             'tasks',
-            'sections')
+            'sections',
+            'colors')
 
-AttribDict={'projects':'(ID TEXT PRIMARY KEY,TITLE TEXT,COLOR )',
-            'labels':2,
-            'tasks':2,
-            'sections':2}
+AttribDict={
+    'projects':'id INTEGER PRIMARY KEY, parentid INTEGER, title TEXT NOT NULL, color TEXT NOT NULL, sectioncount INTEGER NOT NULL, projectcount INTEGER NOT NULL, projecttemplate INTEGER',
+    'labels':'title TEXT PRIMARY KEY, color TEXT NOT NULL,taskcount INTEGER NOT NULL',
+    'tasks':'taskid INTEGER PRIMARY KEY, title TEXT NOT NULL, parentid INTEGER NOT NULL, sectionid INTEGER, priority INTEGER NOT NULL, color INTEGER NOT NULL, tasktemplate INTEGER',
+    'sections':'sectionid INTEGER PRIMARY KEY, title TEXT NOT NULL, taskcount INTEGER NOT NULL, parentprojectid INTEGER NOT NULL, sectiontemplate INTEGER',
+    'colors':'level INTEGER PRIMARY KEY, hexcode TEXT NOT NULL',
+    }
 
-#
+for table in CheckList:                                 #For the tables that have to be checked
+    if CheckAndCreate(Cursor,table,AttribDict[table]):  #iterate through each one and ensure the tables exist
+        pass                                            #If they exist, do nothing
+    else:
+        ErrorLog(f"Could not create TABLE {table} with Attributes {AttribDict[table]}")                                 #If they don't exist, log which one doesn't exist and shut the whole thing down
+        ...
+    
 
 
 
