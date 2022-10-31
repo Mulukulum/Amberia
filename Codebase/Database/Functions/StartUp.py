301bedb7b257e8c8b739b1f967cfa4f95a69f8f5
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-10-31 19:09:11 +0530

    Update Files

--- Codebase/Database/Functions/StartUp.py
+++ Codebase/Database/Functions/StartUp.py
@@ -1,10 +1,19 @@
 
 from Codebase.Database import DataBasePath
 #The following function checks if a table exists in a database
-def Check(cur,name):
+def CheckAndCreate(cur,name,attributes=None):
     cur.execute(f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{name}';""")        #Checks if the table exists
     if cur.fetchall()==[]:              #If No matches were found, (i.e table doesn't exist)
-        return False                    #return False
+        if _CreateTable(cur,name,attributes)==None:
+            return True
     else:
         return True                     #return True if the table exists
+
+#Function to create a table
+def _CreateTable(cur,name,attributes):
+    try:
+        cur.execute(f"""CREATE TABLE {name}""")
+    except:
+        return False
+
 
--- Codebase/Database/Initialize.py
+++ Codebase/Database/Initialize.py
@@ -1,8 +1,56 @@
 import sqlite3
 from Codebase.Database import DataBasePath      #Gets the path of the database on the machine
-from Codebase.Database.Functions.StartUp import Check
+from Codebase.Database.Functions.StartUp import CheckAndCreate
 Con=sqlite3.connect(DataBasePath)       #Creates the database file if it doesn't exist already and makes a connection
 Cursor=Con.cursor()
+
+'''
+LIST OF TABLES
+
+The following is a list of all tables that should be present in the Database
+
+projects
+Attributes :-
+ID Text PRIMARY KEY
+ParentID Text
+
+
+labels
+Attributes:-
+
+
+tasks
+Attributes:-
+
+
+sections
+Attributes:-
+
+
+
+
+
+'''
+#Check if the Following Tables exist
+CheckList=('projects',
+            'labels',
+            'tasks',
+            'sections')
+
+AttribDict={'projects':'(ID TEXT PRIMARY KEY,TITLE TEXT,COLOR )',
+            'labels':2,
+            'tasks':2,
+            'sections':2}
+
+#
+
+
+
+
+
+
+
+
 
 Con.commit()
 Con.close()
