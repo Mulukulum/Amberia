c22f28e5cbabdfd5459df7e09f4bec385a6c5e32
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-01 12:22:42 +0530

    Update __init__.py

--- Codebase/Database/__init__.py
+++ Codebase/Database/__init__.py
@@ -3,12 +3,8 @@ def CheckForError():
     global InitSuccess
     import Codebase.Database.Initialize as Initialize
     if Initialize.CriticalError==True:
-        
         InitSuccess=False
     else:
-        print('3',Initialize.CriticalError)
         InitSuccess=True
     return Initialize.GetPathDatabase()
 DataBasePath=CheckForError()
-print('1',DataBasePath)
-print('2',InitSuccess)
