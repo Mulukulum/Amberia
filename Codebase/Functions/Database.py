266dfda09a0af276468c61181fdca4889c12d5a7
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-03 23:08:57 +0530

    Update Database.py

--- Codebase/Functions/Database.py
+++ Codebase/Functions/Database.py
@@ -1,39 +1,39 @@
-import sqlite3
-from Codebase.Variables import DataBasePath
-ConnectionObject=sqlite3.connect(DataBasePath)
-def ExecuteCommand(cmd,iterable=None):
+import sqlite3                                      
+from Codebase.Variables import DataBasePath         #Get the location of the Database
+ConnectionObject=sqlite3.connect(DataBasePath)      #Creates the connection to the databse
+def ExecuteCommand(cmd,iterable=None):              #Takes a cmd string and an optional iterable to replace a placeholder
     global ConnectionObject
-    cursor=ConnectionObject.cursor()
+    cursor=ConnectionObject.cursor()                #Creates cursor object
     if iterable==None:
         result=cursor.execute(cmd)
     else:
         result=cursor.execute(cmd,iterable)
-    result=result.fetchall()
-    ConnectionObject.commit()
-    return result
-
-def ExecuteManyCommands(cmd,iterable=None):
-    global ConnectionObject
-    cursor=ConnectionObject.cursor()
+    result=result.fetchall()                        #Fetches results
+    ConnectionObject.commit()                       #Commits the database
+    return result                                   #Returns the list
+    
+def ExecuteManyCommands(cmd,iterable=None):         #Takes a cmd string and an optional iterable for placeholders
+    global ConnectionObject                             
+    cursor=ConnectionObject.cursor()                #Creates cursor object
     if iterable==None:
         result=cursor.executemany(cmd)
     else:
         result=cursor.executemany(cmd,iterable)
-    result=result.fetchall()
-    ConnectionObject.commit()
-    return result
+    result=result.fetchall()                        #Fetches results
+    ConnectionObject.commit()                       #Commits the database
+    return result                                   #Returns the list
 
 def ExecuteScript(cmd):
     global ConnectionObject
-    cursor=ConnectionObject.cursor()
-    result=cursor.executescript(cmd)
-    result=result.fetchall()
-    ConnectionObject.commit()
-    return result
+    cursor=ConnectionObject.cursor()                #Creates cursor
+    result=cursor.executescript(cmd)                #Executes the script
+    result=result.fetchall()                        #Returns the list
+    ConnectionObject.commit()                       #Commits changes
+    return result                                   #Returns the list
 
 def CloseConnection():
-    global ConnectionObject
-    ConnectionObject.close()
-    ConnectionObject=None
+    global ConnectionObject     
+    ConnectionObject.close()                        #Closes the connection
+    ConnectionObject=None                           #Sets the conn
 
 
