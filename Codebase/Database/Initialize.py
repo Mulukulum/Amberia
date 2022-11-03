6370d37317f597207e3736b008a8ef8fe104a249
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-03 19:37:15 +0530

    B

--- Codebase/Database/Initialize.py
+++ Codebase/Database/Initialize.py
@@ -81,7 +81,5 @@ Con.commit()
 Con.close()
 if CriticalError:
     ErrorLog("CRITICAL: Database could not be initialised. Check Start Logs for more information")
-from Codebase.Database.Functions.Connector import EntryPoint
-Connection=EntryPoint(DataBasePath)
 #Now that all tables have been created, database can have values put into it.
 #Initialization of Database is done at this point.
