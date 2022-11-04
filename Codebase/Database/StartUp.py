335b8d1f75c06d33ce4ad662b2d1ffb6ef53f417
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-04 13:20:24 +0530

    Update StartUp.py

--- Codebase/Database/StartUp.py
+++ Codebase/Database/StartUp.py
@@ -1,17 +1,20 @@
 
 #The following function checks if a table exists in a database
-def CheckAndCreate(cur,name,attributes):
+def CheckAndCreate(cur,name: str,attributes: str):
     cur.execute(f"""SELECT name FROM sqlite_master WHERE type='table' AND name='{name}';""")  #Checks if the table exists
     if cur.fetchall()==[]:              #If No matches were found, (i.e table doesn't exist)
-        if _CreateTable(cur,name,attributes)==None:
+        if _CreateTable(cur,name,attributes)==True:
             return True
+        else:
+            return False
     else:
         return True                     #return True if the table exists
 
 #Function to create a table
-def _CreateTable(cur,name,attributes):
+def _CreateTable(cur,name: str,attributes: str):
     try:
         cur.execute(f"""CREATE TABLE {name}({attributes});""")
+        return True
     except:
         return False
 
