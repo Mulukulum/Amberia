d4552cd2f24a3c783abfd60aa1b6c179b9816427
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-22 07:17:31 +0530

    Update ui

--- GUI/Test.py
+++ GUI/Test.py
@@ -1,14 +1,23 @@
-from PyQt5 import QtWidgets,QtCore,QtGui,uic
+from PyQt5 import QtWidgets,QtGui,uic,QtCore
 import sys
 
 class Window(QtWidgets.QWidget):
     
-    def __init__(self) -> None:
+    def __init__(self,Tasks:list=[]) -> None:
         super(Window,self).__init__()
-        uic.loadUi('rough.ui',self)
+        uic.loadUi('GUI//UI Files//rough.ui',self)
+        for task in Tasks:
+            self.AddTask(task)
+    
+    def AddTask(self,item:str) -> None:
+        task=QtWidgets.QListWidgetItem(item)
+        task.setFlags(task.flags()|QtCore.Qt.ItemIsUserCheckable)
+        task.setCheckState(QtCore.Qt.Unchecked)
+        self.tasklistwidget.addItem(task)
+
 
 app=QtWidgets.QApplication(sys.argv)
-win=Window()
+win=Window(["Finish UI","Task 25","99.75%"])
 win.show()
 sys.exit(app.exec())
 
