1ccd80bc4914860b6e4247ba4c1a2b34b9cbe807
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-23 22:26:02 +0530

    Update classes.py

--- Codebase/Classes/classes.py
+++ Codebase/Classes/classes.py
@@ -9,81 +9,81 @@ from Codebase.Functions.Database import ExecuteCommand,ExecuteScript
 from Codebase.Functions.Colors import GetRandomColor
 
 class Project:
-    def __init__(self, name, color=None, projects = list(), parentprojects = list()):
-        self.name = name                 #Initialize name of project
-        self.color = color               #Initialize display color
-        self.sections = list()               #Initialize list of sections
-        defaultsection = Section(f"_{self.name}")
-        defaultsection.set_project(self)
-        self.sections.append(defaultsection)
-        self.subprojects = projects           #Initialize list of sub projects 
-        self.parentprojects = parentprojects  #Initialize list of parent projects 
+    def __init__(self, Name, Color=None, Projects = list(), ParentProjects = list()):
+        self.Name = Name                 #Initialize name of project
+        self.Color = Color               #Initialize display color
+        self.Sections = list()               #Initialize list of sections
+        DefaultSection = Section(f"_{self.Name}")
+        DefaultSection.SetProject(self)
+        self.Sections.append(DefaultSection)
+        self.SubProjects = Projects           #Initialize list of sub projects 
+        self.parentprojects = ParentProjects  #Initialize list of parent projects 
 
-    def set_name(self, name):                 #Set name of project
-        self.name = name
+    def SetName(self, name):                 #Set name of project
+        self.Name = name
 
-    def set_color(self, color):
-        self.color = color
+    def SetColor(self, color):
+        self.Color = color
 
-    def add_section(self, newsection):
-        self.sections.append(newsection)
+    def AddSection(self, newsection):
+        self.Sections.append(newsection)
 
-    def remove_section(self, delsection):
-        self.sections.remove(delsection)
+    def RemoveSection(self, delsection):
+        self.Sections.remove(delsection)
 
-    def add_project(self, newproject):
-        self.subprojects.append(newproject)
+    def AddProject(self, newproject):
+        self.SubProjects.append(newproject)
 
-    def remove_project(self, delproject):
-        self.subprojects.remove(delproject)
+    def RemoveProject(self, delproject):
+        self.SubProjects.remove(delproject)
 
-    def add_parentproject(self, newparent):
+    def AddParentProject(self, newparent):
         self.parentprojects.append(newparent)
 
-    def remove_parentproject(self, delparent):
+    def RemoveParentProject(self, delparent):
         self.parentprojects.remove(delparent)
 
-    def display_sections(self):
-        print(*self.sections,sep='\n')
+    def DisplaySections(self):
+        print(*self.Sections,sep='\n')
 
-    def display_projects(self):
-        print(*self.subprojects,sep='\n')
+    def DisplayProjects(self):
+        print(*self.SubProjects,sep='\n')
 
-    def display_parentprojects(self):
+    def DisplayParentProjects(self):
         print(*self.parentprojects,sep='\n')
           
 
     def __str__(self):
-        return  f'Project name: {self.name} \
-                \nDisplay color: {self.color} \
-                \nSections: {[str(s) for s in self.sections]}'
+        return  f'Project name: {self.Name} \
+                \nDisplay color: {self.Color} \
+                \nSections: {[str(s) for s in self.Sections]}'
 
     def __repr__(self):
-        return f"Project({self.name},{self.color},{self.sections},{self.subprojects},{self.parentprojects})"
+        return f"Project({self.Name},{self.Color},{self.Sections},{self.SubProjects},{self.parentprojects})"
 
 class Section:
-    def __init__(self, name, project=None, tasks=list()):
-        self.name = name                 #Initialize name of section
-        self.project = project
-        self.tasks = tasks               #Initialize a list of tasks
+    def __init__(self, Name, Project: Project, Tasks: list=list()):
+        self.Name = Name                 #Initialize name of section
+        self.Project = Project
+        self.Tasks = Tasks               #Initialize a list of tasks
 
-    def set_project(self, newproject):   #Set the project to which the section belongs
-        self.project = newproject         
+    def SetProject(self, NewProject):   #Set the project to which the section belongs
+        self.Project = NewProject         
 
-    def add_task(self, newtask):         #Add a new task to the section
-        self.tasks.append(newtask)
+    def AddTask(self, NewTask):         #Add a new task to the section
+        self.Tasks.append(NewTask)
 
-    def remove_task(self, deltask):      #Remove a task from the list of tasks
-        self.tasks.remove(deltask)
+    def RemoveTask(self, DelTask):      #Remove a task from the list of tasks
+        self.Tasks.remove(DelTask)
 
-    def display_tasks(self):             #Display the list of tasks
-        print(*self.tasks,sep='\n')      #Displays tasks without a for loop         
+    def DisplayTasks(self):             #Display the list of tasks
+        print(*self.Tasks,sep='\n')      #Displays tasks without a for loop         
 
     def __repr__(self):
-        return f"Section({self.name},{self.project},{self.tasks})"              
+        return f"Section({self.Name},{self.Project},{self.Tasks})"              
 
     def __str__(self):
-        return f'Section name: {self.name} \nProject name: {self.project.name} \nTasks: {[str(t) for t in self.tasks]}'        
+        return f'Section name: {self.Name} \nProject name: {self.Project.name} \nTasks: {[str(t) for t in self.Tasks]}'        
         #fstring returns the string representation
 
 class Label:
@@ -260,13 +260,13 @@ class Priority:
         self.PriorityLevel=NewLevel           #Enforcing an official unofficial rule that:
         self.Color=self.ColorCache[NewLevel]  #ColorCache must always be populated
 
-class task:
-    def __init__(self, TaskTitle, TaskDesc=None, priority=None, DueDate=None, Labels=None): #Initializes the class
+class Task:
+    def __init__(self, TaskTitle, TaskDesc=None, PriorityLevel=10, DueDate=None, Labels=None): #Initializes the class
         self.TaskTitle=TaskTitle
         self.TaskDesc=TaskDesc
         self.DueDate=DueDate
-        if Priority.IsValidPriority(priority):          #Checks if the incoming argument is a valid priority level
-            self.priority=Priority(priority)            #If so, then give the task its priority
+        if Priority.IsValidPriority(PriorityLevel):          #Checks if the incoming argument is a valid priority level
+            self.priority=Priority(PriorityLevel)            #If so, then give the task its priority
         else:
             self.priority=Priority(10)                  #If not, then set it to a default value of 10
             Log(f"Task {self.TaskTitle} given no priority. Default Value Assigned")
