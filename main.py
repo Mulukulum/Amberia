240487cb7cc92df5189fc02c5fcd61aef79a36d5
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-24 13:47:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#############################         PROGRESS        ####################################
+
+# The backend is now nearly done, with only tasks left to clean up
+# As for the UI, Widgets for pretty much everything are almost done, only thing left is 
+# to add each widget to QScrollArea, QHBoxLayout and QVBoxLayouts and UI will be finished
+# Functions for connecting the backend to the ui haven't had any work started on them
+
+#Setup the Classes
+from Codebase.Classes import classes
+
+# Also rohit if you're reading this I couldn't do the UI stuff yet, sorry
+# I'll work on them tonight and this'll mostly be done by this saturday
+# which leaves 2 days for fixing bugs and adding any last minute features
