a30f4f2b89981063fb5c9c7857e18e930224d75f
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-27 19:56:53 +0530

    Adding Tasks Support

--- Codebase/GUI/Widgets.py
+++ Codebase/GUI/Widgets.py
@@ -26,12 +26,18 @@ class TodayTasksWidget(QtWidgets.QWidget):
 
 class TaskWidget(QtWidgets.QWidget):
 
+    #Lambda Function because python somehow doesn't have a method for this
+    OrdinalTimeFunction=lambda n : str(n) + {1:'st',2:'nd',3:'rd'}.get(abs(n)%10,'th')
+
     def __init__(self,frame) -> None:
         super().__init__(frame)
         self.ui=TaskWidgetUI()
         self.ui.setupUi(frame)
     
     def SetInformation(self,TaskObject: cl.Task):
+        
+        #Sets the name of the object for easy identification
+        self.setObjectName(f"TaskWidget{TaskObject.ID}")
         
         #Sets the Display to show priority Level
         self.ui.PriorityLevelDisplay.display(TaskObject.PriorityLevel)
@@ -44,8 +50,36 @@ class TaskWidget(QtWidgets.QWidget):
         
         #Get the Title of the Task
         Title=TaskObject.TaskTitle
+        Due=TaskObject.DueDate
+        Completed=TaskObject.Completed
 
         #If the task is given a duedate
-        if TaskObject.DueDate==None:
+        if Due==None:
+
+            #Then Hide the days left part
             self.ui.DaysLeftDisplay.hide()
             self.ui.DaysLeftLabel.hide()
+            Text=Title
+        
+        else:
+            if not Completed:
+                Text=f"Due On {Due.strftime(f'%a, {TaskWidget.OrdinalTimeFunction(Due.day)} %b %Y')} "+Title
+                
+            else:
+                Text=f"Task Finished at {TaskObject.CompletedDate.strftime('%c')}"
+
+                #We can hide these labels since there's no need for a days left counter
+                self.ui.DaysLeftDisplay.hide()
+                self.ui.DaysLeftLabel.hide()
+        
+        #Set the Title text
+        self.ui.TaskTitle_label.setText(Text)
+
+        #Show the labels if they're meant to be shown
+        if 1:
+            self.ui.DaysLeftDisplay.show()
+            self.ui.DaysLeftLabel.show()
+
+
+        
+        
