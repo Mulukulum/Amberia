3e84e69708895cd49468479b0c3a324e9987de04
Mulukulum <114818131+Mulukulum@users.noreply.github.com>
2022-11-29 00:44:27 +0530

    Update ThreadsForReminders.py

--- Codebase/Classes/ThreadsForReminders.py
+++ Codebase/Classes/ThreadsForReminders.py
@@ -4,7 +4,24 @@ import threading
 import datetime
 import concurrent.futures as cf
 import Codebase.Classes.classes as cl
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from Codebase.ErrorLogs.logging import ErrorLog
 
+class EmitNotificationThread(QtCore.QObject):
+    
+
+
+    def __init__(self) -> None:
+        pass
+
+    @QtCore.pyqtSlot(datetime.datetime)
+    def StartThread(self,Date: datetime.datetime):
+        #If the date is less than the current time then just return
+        if Date<=datetime.datetime.now():
+            ErrorLog
+            return
+        ...
 
 def main():
     ...
